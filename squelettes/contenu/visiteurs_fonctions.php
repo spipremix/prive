dcb31d66deffc84460d8111ec9877041a9e846ad
Cerdic <cedric@yterium.com>
2010-10-05 12:36:49 +0000

    squeletisation complete des pages auteurs et visiteurs
    git-svn-id: file:///home/spipremix/migrations/targets/repositories/svn/spip/spip@16417 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ squelettes/contenu/visiteurs_fonctions.php
@@ -0,0 +1,18 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2010                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+// Constante pour le nombre d'auteurs par page.
+@define('AUTEURS_DEFAUT', '!');
+include_spip('prive/squelettes/contenu/auteurs_fonctions');
+
+?>
